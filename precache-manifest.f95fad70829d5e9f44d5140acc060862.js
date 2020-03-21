e2077bbe4351eb5424df9825a424f366680d964a
yassine <yassine.lf.99@gmail.com>
2020-03-21 01:38:44 +0100

    first commit

---
+++ precache-manifest.f95fad70829d5e9f44d5140acc060862.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23a314ffe0c3a184bc26169c7f7d48a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87454d182dc009fe76d4",
+    "url": "/static/css/main.81103024.chunk.css"
+  },
+  {
+    "revision": "f48dac99d295908142e6",
+    "url": "/static/js/2.787306a5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.787306a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87454d182dc009fe76d4",
+    "url": "/static/js/main.55c383f4.chunk.js"
+  },
+  {
+    "revision": "32108ac31ef0bb80b0e1",
+    "url": "/static/js/runtime-main.3bc3a39b.js"
+  }
+]);
